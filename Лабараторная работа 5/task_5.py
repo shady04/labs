94d655576d8746d86f8213710979ded3dd49234b
shady04 <119017323+shady04@users.noreply.github.com>
2022-12-11 16:43:25 +0300

    Лабораторная работа 5

---
+++ Лабараторная работа 5/task_5.py
@@ -0,0 +1,15 @@
+import random
+
+def get_random_password() -> str:
+
+    a = "abcdefghjklmnopqrstuvwxyz"
+    A = "ABCDEFGJKLMNOPQRSTUVWXYZ"
+    num = '0123456789'
+    b = a + A + num
+    return random.sample(b, 8)
+
+
+
+
+print(get_random_password())
+#спасибо за тщательную проверку работ!
